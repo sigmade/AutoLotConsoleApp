761b67073bf16e08fb582de7886938667c60f728
sigmade <sychyov1991@mail.ru>
2020-05-21 14:14:25 +0300

    Add project files.

---
+++ AutoLotConsoleApp/EF/AutoLotEntities.cs
@@ -0,0 +1,28 @@
+namespace AutoLotConsoleApp.EF
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class AutoLotEntities : DbContext
+    {
+        public AutoLotEntities()
+            : base("name=AutoLotEntities")
+        {
+        }
+
+        public virtual DbSet<CreditRisk> CreditRisks { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Inventory> Inventories { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Inventory>()
+                .HasMany(e => e.Orders)
+                .WithRequired(e => e.Inventory)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
